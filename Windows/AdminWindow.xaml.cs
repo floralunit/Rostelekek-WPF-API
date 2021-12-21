7b8863ab12ebd51a1b943970eb3e1af9b4304f1d
Anya-jpg <71765648+floralunit@users.noreply.github.com>
2021-12-22 01:31:52 +0300

    Added AdminWindow front-end

---
+++ Windows/AdminWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Rostelekek_WPF_API.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+        public AdminWindow()
+        {
+            InitializeComponent();
+        }
+        private void BEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void BDelete_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void BCreate_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void BExit_Click(object sender, RoutedEventArgs e)
+        {
+            new LoginWindow().Show();
+            Close();
+        }
+    }
+}
